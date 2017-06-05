13ef65866c8430446e9b247fa7a48b235d8b52f7
AnTi3z <anti3z@anti3z.ru>
2017-06-05 16:29:36 +0300

    str_human_int fix

--- tools.py
+++ tools.py
@@ -1,11 +1,12 @@
 def str_human_int (x):
-    if x < 10000: return '%d' % x
+    if abs(x) < 10000: return '%d' % x
     i = 0
-    while round(x) >= 1000 and i < 3:
+    while abs(round(x)) >= 1000 and i < 3:
         x /= 1000
         i += 1
     suffix = ['','k','M','G'][i]
 
-    if round(x) >= 100 or suffix == '': return '{:.0f}{}'.format(x,suffix)
-    elif round(x) >= 10: '{:.1f}{}'.format(x,suffix)
+    if abs(round(x)) >= 100 or suffix == '': return '{:.0f}{}'.format(x,suffix)
+    elif abs(round(x)) >= 10: return '{:.1f}{}'.format(x,suffix)
     else: return '{:.2f}{}'.format(x,suffix)
+
