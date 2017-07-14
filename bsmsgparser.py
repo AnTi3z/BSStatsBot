1e0caf76a00cc3b0a975ce233e89353e788648d2
AnTi3z <anti3z@anti3z.ru>
2017-07-14 14:33:17 +0300

    Pricompiled regexps. Access level fixes.

--- bsmsgparser.py
+++ bsmsgparser.py
@@ -2,17 +2,17 @@ import re
 import statdb
 import logging
 from out_fmts import battle_stat_fmt
-from tools import get_acc_lvl
 
 logger = logging.getLogger('BSstatbot')
 
+trg_regexp = re.compile(r'расположился \U0001f5e1?\U0001f608?(?:\[(.+?)\u200b?\])?(.+) в своих владениях (.+) размером \d+.+За победу ты получишь -?\d+.+')
+btl_regexp = re.compile(r'Битва с (?!альянсом )(?:\U0001f5e1)?(?:\U0001f608)?(?:\[(.+?)\u200b?\])?(?P<user_name2>.+) окончена\. (?:(?P<win_flag>Поздравляю)|К сожалению), (?P<user_name1>.+)(?:, твоя|! Твоя).+? (?:Никто из|(?P<return_army>\d+). (?:из|без)) (?P<send_army>\d+)?(?:(?:.+награда составила |.+казна опустела на )(?P<money>\d+))?(?:.+ (?P<land>\d+). отошли (?:к твоим владениям\.|(?P<land_name2>.+)\.))?(?:.+изменилась на (?P<karma>-?\d+))?.*', re.S)
 ally_list_regexp = re.compile(r'\[(.+?)\u200b?\](.+) / (\d+) / \U0001f5e1?\U0001f608?(.+)')
 
-def bs_fwd_parser(msg):
+
+def bs_fwd_parser(msg, acc_lvl):
     # Разведка
-    trg = re.search(
-        r'расположился \U0001f5e1?\U0001f608?(.+) в своих владениях (.+) размером \d+.+\nЗа победу ты получишь -?\d+.+\n',
-        msg.text)
+    trg = trg_regexp.search(msg.text)
     if trg:
         statdb.new_game_user(trg.group(1))
         statdb.update_user_land(trg.group(1), trg.group(2))
@@ -24,9 +24,7 @@ def bs_fwd_parser(msg):
             return
 
     # Сражение
-    btl = re.search(
-        r'Битва с (?P<user_name2>.+) окончена\. (?:(?P<win_flag>Поздравляю)|К сожалению), (?P<user_name1>.+)(?:, твоя|! Твоя).+ (?:Никто из|(?P<return_army>\d+). из) (?P<send_army>\d+)(?:(?:.+с наградой |.+разграблена на )(?P<money>\d+))?(?:.+ (?P<land>\d+). отошли (?:к твоим владениям\.|(?P<land_name2>.+)\.))?(?:.+изменилась на (?P<karma>-?\d+))?.*',
-        msg.text)
+    btl = btl_regexp.search(msg.text)
     if btl:
         statdb.new_game_user(btl.group('user_name2'))
         statdb.new_game_user(btl.group('user_name1'), msg.from_user.id)
