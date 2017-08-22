390695245aaaa27767f2c1ac9121a06429b5c9ea
AnTi3z <anti3z@anti3z.ru>
2017-08-22 21:53:20 +0300

    Parser fix

--- bsmsgparser.py
+++ bsmsgparser.py
@@ -5,9 +5,9 @@ from out_fmts import battle_stat_fmt
 
 logger = logging.getLogger('BSstatbot')
 
-trg_regexp = re.compile(r'расположился \U0001f5e1?\U0001f608?(?:\[(.+?)\u200b?\])?(.+) в своих владениях (.+) размером \d+.+За победу ты получишь -?\d+.+')
-btl_regexp = re.compile(r'Битва с (?!альянсом )(?:\U0001f5e1)?(?:\U0001f608)?(?:\[(.+?)\u200b?\])?(?P<user_name2>.+) окончена\. (?:(?P<win_flag>Поздравляю)|К сожалению), (?P<user_name1>.+)(?:, твоя|! Твоя).+? (?:Никто из|(?P<return_army>\d+). (?:из|без)) (?P<send_army>\d+)?(?:(?:.+награда составила |.+казна опустела на )(?P<money>\d+))?(?:.+ (?P<land>\d+). отошли (?:к твоим владениям\.|(?P<land_name2>.+)\.))?(?:.+изменилась на (?P<karma>-?\d+))?.*', re.S)
-ally_list_regexp = re.compile(r'\[(.+?)\u200b?\](.+) / (\d+) / \U0001f5e1?\U0001f608?(.+)')
+trg_regexp = re.compile(r'расположился \U0001f5e1?\U0001f608?(?:\[(.+?)\u200b*\])?(.+) в своих владениях (.+) размером \d+.+За победу ты получишь -?\d+.+')
+btl_regexp = re.compile(r'Битва с (?!альянсом )(?:\U0001f5e1)?(?:\U0001f608)?(?:\[(.+?)\u200b*\])?(?P<user_name2>.+) окончена\. (?:(?P<win_flag>Поздравляю)|К сожалению), (?P<user_name1>.+)(?:, твоя|! Твоя).+? (?:Никто из|(?P<return_army>\d+). (?:из|без)) (?P<send_army>\d+)?(?:(?:.+награда составила |.+казна опустела на )(?P<money>\d+))?(?:.+ (?P<land>\d+). отошли (?:к твоим владениям\.|(?P<land_name2>.+)\.))?(?:.+изменилась на (?P<karma>-?\d+))?.*', re.S)
+ally_list_regexp = re.compile(r'\[(.+?)\u200b*\](.+) / (\d+) / \U0001f5e1?\U0001f608?(.+)')
 
 
 def bs_fwd_parser(msg, acc_lvl):
@@ -32,7 +32,8 @@ def bs_fwd_parser(msg, acc_lvl):
     if btl:
         statdb.new_game_user(btl.group('user_name2'))
         statdb.new_game_user(btl.group('user_name1'), msg.from_user.id)
-        if not btl.group('win_flag'): statdb.update_user_land(btl.group('user_name2'), btl.group('land_name2'))
+        if not btl.group('win_flag') and btl.group('land_name2'):
+            statdb.update_user_land(btl.group('user_name2'), btl.group('land_name2'))
         statdb.new_battle(msg, **btl.groupdict())
         return
 
